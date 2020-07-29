0b521c58723fe99c0807d7b941cdb7a2647b189e
Fernando Medina Corey <fmcorey@gmail.com>
2020-07-28 22:25:18 -0700

    random changes to lots of stuff for frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <PageHeader/>
+    <div class="ui container main-box">
+    </div>
+  </div>
+</template>
+
+<script>
+import PageHeader from './components/PageHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PageHeader, 
+  },
+}
+</script>
+
+<style>
+  .card {
+    margin-top: auto;
+  }
+  body {
+    background-color: rgb(164, 199, 230);
+  }
+  .main-box {
+    margin-top: 6rem;
+  }
+  body>.container {
+    padding: 24px;
+    background-color: rgb(164, 199, 230);
+  }
+  .hits-box {
+    padding: 1px 14px 12px;
+  }
+
+body {    
+    padding: 1em;
+  }
+  
+  .ui.action.input input[type="file"] {
+    display: none;
+  }
+  
+  .ui.top.fixed {
+    background: rgb(225, 212, 168);
+  }
+
+  .ui.horizontal.divider {
+    color: rgb(225, 212, 168);
+  }
+</style>
---
+++ frontend/src/components/PageHeader.vue
@@ -0,0 +1,65 @@
+<template>
+  <div v-if="!$auth.loading" class="ui top fixed horizontal menu">
+    <div class="item">
+      <h2>The Survey Union</h2>
+    </div>
+    <button v-on:click="login()" v-if="!$auth.isAuthenticated" class="ui primary button">Login</button>
+    <button v-on:click="logout()" v-if="$auth.isAuthenticated" class="ui primary button">Logout</button>
+    <div id="avatar-img-div" v-show="profileImageVisibility">
+      <img :src="profileUrl" class="ui avatar image"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageHeader',
+  methods: {
+    login() {
+      this.$auth.loginWithRedirect();
+    },
+    logout() {
+      this.$auth.logout({
+        returnTo: window.location.origin
+      });
+    },
+  },
+  data () {
+    return {
+      profileImageVisibility: false,
+      profileUrl: '',
+    }
+  }
+}
+</script>
+
+<style scoped>
+.ui.top.fixed {
+  background: rgb(225, 212, 168);
+  margin-bottom: 100px;
+}
+
+.ui.modal {
+  top: 10%;
+}
+
+.close.icon {
+  color: black;
+  top: 0;
+  right: 0;
+}
+
+.hidden {
+  display: none;
+}
+
+label {
+  margin-bottom: 10px;
+  display: block;
+}
+
+body {
+  background: #1B1C1D;
+}
+
+</style>
